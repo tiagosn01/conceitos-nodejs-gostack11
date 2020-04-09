afda50d3bc2f9a65ad48e872fbf95bd7721892de
tiagosn01 <tiagosilva0922@gmail.com>
2020-04-08 22:22:32 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,80 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body
+
+  const repository = repositories.find(repository => repository.id === id)
+
+
+  if (!repository) {
+    return response.status(400).json({error: 'Repository not exists'})
+  }
+
+  const newRepo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories[repository.id] = newRepo;
+
+  return response.json(newRepo)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id)
+
+
+  if (!repository) {
+    return response.status(400).json({error: 'Repository not exists'})
+  }
+
+  repositories.splice(repository.id)
+
+  return response.status(204).send() 
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(400).json({ error: 'Repository not existis.' })
+  }
+
+  repository.likes += 1
+
+  return response.json(repository);
 });
 
 module.exports = app;
